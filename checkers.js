ba92f38a9f2e51ba8902700c11413ed14c2b55ac
Michael Giebner <mikegiebner@gmail.com>
2019-09-12 17:43:15 -0400

    Version 9 complete

--- checkers.js
+++ checkers.js
@@ -1,4 +1,4 @@
-// Odd i = dark piece move while even i = light piece move
+// Odd i = dark piece move while even i = lite piece move
 var i = 1
 
 var mouseDown = document.getElementById("myTable")
@@ -19,17 +19,17 @@ stop = mouseUp.addEventListener("mouseup", function(e) {
 
 var game = {
   initPieces: function() {
-    var darkPieces  = []
-    var litePieces = []
+    this.darkPieces  = []
+    this.litePieces = []
 
     // Create 9 generic game pieces for each side
     for ( var i = 0; i < 12; i++ ) {
-      darkPieces.push({
+      this.darkPieces.push({
         square: "0",
         color: "dark",
         type: "regPiece"
       })
-      litePieces.push({
+      this.litePieces.push({
         square: "0",
         color: "lite",
         type: "regPiece"
@@ -37,34 +37,34 @@ var game = {
     }
 
     // Position each piece in a starting position
-    darkPieces[0].square  = "11"
-    darkPieces[1].square  = "13"
-    darkPieces[2].square  = "15"
-    darkPieces[3].square  = "17"
-    darkPieces[4].square  = "22"
-    darkPieces[5].square  = "24"
-    darkPieces[6].square  = "26"
-    darkPieces[7].square  = "28"
-    darkPieces[8].square  = "31"
-    darkPieces[9].square  = "33"
-    darkPieces[10].square = "35"
-    darkPieces[11].square = "37"
-    litePieces[0].square  = "62"
-    litePieces[1].square  = "64"
-    litePieces[2].square  = "66"
-    litePieces[3].square  = "68"
-    litePieces[4].square  = "71"
-    litePieces[5].square  = "73"
-    litePieces[6].square  = "75"
-    litePieces[7].square  = "77"
-    litePieces[8].square  = "82"
-    litePieces[9].square  = "84"
-    litePieces[10].square = "86"
-    litePieces[11].square = "88"
+    this.darkPieces[0].square  = "11"
+    this.darkPieces[1].square  = "13"
+    this.darkPieces[2].square  = "15"
+    this.darkPieces[3].square  = "17"
+    this.darkPieces[4].square  = "22"
+    this.darkPieces[5].square  = "24"
+    this.darkPieces[6].square  = "26"
+    this.darkPieces[7].square  = "28"
+    this.darkPieces[8].square  = "31"
+    this.darkPieces[9].square  = "33"
+    this.darkPieces[10].square = "35"
+    this.darkPieces[11].square = "37"
+    this.litePieces[0].square  = "62"
+    this.litePieces[1].square  = "64"
+    this.litePieces[2].square  = "66"
+    this.litePieces[3].square  = "68"
+    this.litePieces[4].square  = "71"
+    this.litePieces[5].square  = "73"
+    this.litePieces[6].square  = "75"
+    this.litePieces[7].square  = "77"
+    this.litePieces[8].square  = "82"
+    this.litePieces[9].square  = "84"
+    this.litePieces[10].square = "86"
+    this.litePieces[11].square = "88"
 
     for ( var i = 0; i < 12; i++ ) {
-      this.displayPiece( darkPieces[i] )
-      this.displayPiece( litePieces[i] )
+      this.displayPiece( this.darkPieces[i] )
+      this.displayPiece( this.litePieces[i] )
     }
 
   },
@@ -82,15 +82,53 @@ var game = {
   },
   movePiece: function( start, stop ) {
 
+    // Find the piece at the start location
+    // e represents the entire array
+    // e.square is a piece of an array element
+    var piece = this.darkPieces.filter( e => e.square === start )
+
+    // Find the index in the 
+    var index = this.darkPieces.findIndex( e => e.square === start )
+
+    if ( piece.length === 0 ) {
+      piece = this.litePieces.filter( e => e.square === start )
+      index = this.litePieces.findIndex( e => e.square === start )
+    }
+
+    // Check the stop location
+    var stopLocation = document.getElementById(stop)
+
+    if ( stopLocation.className === "lite" ) {
+      return
+    }
+
     var oldSpot = document.getElementById(start)
     var newSpot = document.getElementById(stop)
 
-    if ( oldSpot.textContent === "o" ) {
-      oldSpot.textContent = ""
-      newSpot.textContent = "o"
+    if ( oldSpot === null ) { console.log("You selected a blank spot") }
 
-      newSpot.className = oldSpot.className
-      oldSpot.className = "dark"
+    else if ( oldSpot.textContent === "" ) { console.log("You selected a blank spot") }
+    
+    else if ( oldSpot.textContent === "o" ) {
+
+      // Update location of new spot
+      if ( piece[0].color === "dark" ) { this.darkPieces[index].square = stop }
+      else if ( piece[0].color === "lite" ) { this.litePieces[index].square = stop }
+
+      if ( piece[0].type === "regPiece" ) {
+        // Remove o from previous location
+        oldSpot.textContent = ""
+        // Add o to new location
+        newSpot.textContent = "o"
+        
+        // Update classNames in the DOM
+        newSpot.className = oldSpot.className
+        oldSpot.className = "dark"
+      }
+      else if ( oldSpot.type === "King" ) {
+        console.log("Kimg Me!!")
+      }
+
     }
 
   }
