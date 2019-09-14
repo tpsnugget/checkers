62cd7d5e3da22622ef3b673ac559cfd022690049
Michael Giebner <mikegiebner@gmail.com>
2019-09-14 19:13:15 -0400

    Version 19 complete

---
+++ isLegalSquare.js
@@ -0,0 +1,6 @@
+// The purpose of this file is to encapsulate a large function that checks if a
+// starting square and landing square are a legal combination
+
+function isLegalSquare( start, stop ) {
+   console.log("That is a legal Square")
+}
